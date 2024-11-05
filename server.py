82c4bf5c89c03306dfcabeaab03b4c332c7baf86
Ahmad Hussain <ahmed536h@gmail.com>
2024-11-06 01:25:03 +0500

    Update server.py

--- server.py
+++ server.py
@@ -14,10 +14,8 @@ app = Flask("Sentiment Analysis")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
-    # Pass the text to the sentiment_analyzer function and store the response
     response = sentiment_analyzer(text_to_analyze)
     if response != "Invalid entry! try again":
         label = response['label']
